4e90e37d0544899b029fee917fd6585fc6538425
Andrii Penchuk <penja7@gmail.com>
2024-11-14 11:50:16 +0200

    Update main.tf

--- terrafrom-parent-module/terraform-null-module/main.tf
+++ terrafrom-parent-module/terraform-null-module/main.tf
@@ -1,8 +1,32 @@
 resource "null_resource" "test" {
   count = var.quantity
-  # triggers = {
-  #   trigger = timestamp()
-  # }
+  triggers = {
+     trigger = timestamp()
+     json_data   = jsonencode(var.json_data)
+   }
+}
+
+null_resources_json = {
+  "0" = {
+    "key1" = "value1value1value1value1value1value1value1"
+    "key2" = "value2value2value2value2value2value2value2"
+  }
+  "1" = {
+     "key1" = "value1value1value1value1value1value1value1"
+     "key2" = "value2value2value2value2value2value2value2"
+  }
+  "2" = {
+     "key1" = "value1value1value1value1value1value1value1"
+     "key2" = "value2value2value2value2value2value2value2"
+  }
+  "3" = {
+    "key1" = "value1value1value1value1value1value1value1"
+    "key2" = "value2value2value2value2value2value2value2"
+  }
+  "4" = {
+     "key1" = "value1value1value1value1value1value1value1"
+     "key2" = "value2value2value2value2value2value2value2"
+  }
 }
 
 variable "quantity" {
