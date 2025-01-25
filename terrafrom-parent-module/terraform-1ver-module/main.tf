998ef41a97c0fa1d9d8a91f823fc7939518de1d8
Andrii Penchuk <penja7@gmail.com>
2025-01-25 14:48:29 +0200

    Update main.tf

--- terrafrom-parent-module/terraform-1ver-module/main.tf
+++ terrafrom-parent-module/terraform-1ver-module/main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "some_resource" {
   provisioner "local-exec" {
     command = <<EOT
       echo "Printing Environment Variables:"
-      printenv
+      env
     EOT
   }
 }
