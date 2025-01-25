76fd0c0f1fedecb16e6472d9c175ca95ce55ab81
Andrii Penchuk <penja7@gmail.com>
2025-01-25 14:42:42 +0200

    Update main.tf

--- terrafrom-parent-module/terraform-1ver-module/main.tf
+++ terrafrom-parent-module/terraform-1ver-module/main.tf
@@ -9,11 +9,6 @@ resource "null_resource" "some_resource" {
     EOT
   }
 }
-
-data "external" "print_var" {
-  program = ["bash", "-c", "echo {\"SCALR_SUPPORT_VAR\":\"$SCALR_SUPPORT_VAR\"}"]
-}
-
 variable "some_input" {
   default = "string value"
 }
