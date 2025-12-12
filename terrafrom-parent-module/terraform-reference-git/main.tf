0685540f2b5a70d4dbde033881f8e0b1f80799ae
Andrii Penchuk <penja7@gmail.com>
2025-12-12 14:38:42 +0200

    Update main.tf

--- terrafrom-parent-module/terraform-reference-git/main.tf
+++ terrafrom-parent-module/terraform-reference-git/main.tf
@@ -1,3 +1,9 @@
 module "flat_from_repo" {
   source = "git::https://github.com/aleatoricmbnt/git-nested-01.git"
+
+}
+
+module "flat_from_repo2" {
+  source = "git::https://github.com/aleatoricmbnt/git-nested-01.git"
+
 }
