19c98fa67d9153cbc16860f7670252bbd80f9ac7
Andrii Penchuk <penja7@gmail.com>
2025-12-12 14:41:29 +0200

    Reformat module declarations in main.tf

--- terrafrom-parent-module/terraform-reference-git/main.tf
+++ terrafrom-parent-module/terraform-reference-git/main.tf
@@ -7,3 +7,8 @@ module "flat_from_repo2" {
   source = "git::https://github.com/aleatoricmbnt/git-nested-01.git"
 
 }
+
+module "flat_from_repo3" {
+  source = "git::https://github.com/aleatoricmbnt/git-nested-01.git"
+
+}
