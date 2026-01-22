6607a82e6b48e167378cbdb73cbad3cad9229355
Andrii Penchuk <penja7@gmail.com>
2026-01-22 11:46:50 +0200

    Modify random_pet count and add sleep_time variable
    Increased the count of random pets to 100 and added a sleep_time variable with a default value of 240 seconds.

--- terrafrom-parent-module/terraform-random-pet/main.tf
+++ terrafrom-parent-module/terraform-random-pet/main.tf
@@ -1,6 +1,22 @@
 resource "random_pet" "new" {
-  count = 1
+  count = 100
   keepers = {
     timestamp = timestamp()
+  }
+}
+
+
+variable "sleep_time" {
+  count = 20
+  type        = number
+  default     = 240
+  description = "Sleep duration in seconds"
+}
+
+resource "time_sleep" "wait" {
+  create_duration = format("%ds", var.sleep_time)
+
+  triggers = {
+    always_run = timestamp()
   }
 }
