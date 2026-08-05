e079f1c538d61b2416e09c238147333bc8093d18
Andrii Penchuk <penja7@gmail.com>
2026-08-05 14:14:42 -0700

    Update main.tf

--- config_tests/many_modules/main.tf
+++ config_tests/many_modules/main.tf
@@ -54,19 +54,6 @@ module "mod_008" {
   quantity = 1
 }
 
-module "mod_009" {
-  source   = "git::https://github.com/penja/scalr-testing-module//terrafrom-parent-module/terraform-null-module?ref=master"
-  quantity = 1
-}
 
-module "mod_010" {
-  source   = "git::https://github.com/penja/scalr-testing-module//terrafrom-parent-module/terraform-null-module?ref=master"
-  quantity = 1
-}
-
-module "mod_011" {
-  source   = "git::https://github.com/penja/scalr-testing-module//terrafrom-parent-module/terraform-null-module?ref=master"
-  quantity = 1
-}
 
 
